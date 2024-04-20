18e9130b9e46a6d0106d1a2a861240912d89c7d9
Abhishek Sawant <abhisheksawant28@gmail.com>
2024-04-20 09:39:04 +0530

    Commit

--- src/main/java/Day1/SeleniumDemo.java
+++ src/main/java/Day1/SeleniumDemo.java
@@ -1,21 +1,35 @@
 package Day1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class SeleniumDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
 
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\JetBrains\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("http://www.google.com");
-        System.out.println(driver.getTitle());
+        driver.get("http://www.youtube.com");
 
 
-        ;
+        WebElement searchyoutubetesttribe = driver.findElement(By.name("search_query"));
+
+
+        driver.findElement(By.id("search-icon-legacy")).click();
+
+        driver.findElement(By.xpath("//input[@id='search']")).clear();
+
+        driver.findElement(By.xpath("//input[@id='search']")).sendKeys("Podtest");
+        driver.findElement(By.id("search-icon-legacy")).click();
+
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
